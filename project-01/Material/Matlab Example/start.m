ef12642cdd16b484a9d2fb1e739bc604bac47d4a
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-09-22 21:26:44 +0200

    Added first own example (rainbow)

--- project-01/Material/Matlab Example/start.m
+++ project-01/Material/Matlab Example/start.m
@@ -3,19 +3,21 @@
 
 %% Assignement 1
 
-img = imread('imgs/castle.jpg');
+img = imread('imgs/rainbow.png');
 img = im2double(img);
 
-[grey inverted] = spanishCastle(img);
+[gray inverted] = spanishCastle(img);
 
 % Please show your images with imshow (instead of imwrite) and add titles
 % to the (sub)figures if there are more then one.
 figure(1);
-imshow(grey);
-title('Greyscale image');
+imshow(gray);
+%imwrite(grey,'imgs\rainbow_gray.png')
+title('Gray-scale image');
 
 figure(2)
 imshow(inverted);
+%imwrite(inverted,'imgs\rainbow_inv.png')
 title('Inverted image');
 
 
