bae3f6a02100410131f9d03da41550d649417d30
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-10-02 00:27:20 +0200

    Added example images and stuff to Spanish Castle, extended report.

--- project-01/Material/Matlab Example/start.m
+++ project-01/Material/Matlab Example/start.m
@@ -3,23 +3,21 @@
 
 %% Assignement 1
 
-img = imread('imgs/rainbow.png');
+img = imread('imgs/calanque.jpg');
 img = im2double(img);
 
 [gray inverted] = spanishCastle(img);
 
-% Please show your images with imshow (instead of imwrite) and add titles
-% to the (sub)figures if there are more then one.
 figure(1);
 imshow(gray);
-%imwrite(grey,'imgs\rainbow_gray.png')
 title('Gray-scale image');
 
 figure(2)
 imshow(inverted);
-%imwrite(inverted,'imgs\rainbow_inv.png')
 title('Inverted image');
 
+% imwrite(gray,'imgs\gray.png')
+% imwrite(inverted,'imgs\inv.png')
 
 %% Assignement 2.1
 img = imread('foliage_raw.tiff');
