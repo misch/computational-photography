d0cbd17b009d3c9ee6338c23af77e5a0a500bb9d
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-10-02 00:41:53 +0200

    Corrected implementation of manual white balancing

--- project-01/Material/Matlab Example/balance.m
+++ project-01/Material/Matlab Example/balance.m
@@ -1,30 +1,31 @@
 function [ balanced ] = balance(img)
 
 choice = menu('Choose balancing method','Automatic (grey world assumption)','Manual white balancing');
-avgRGB = shiftdim(mean(mean(img)),1);
 
-if (choice == 1)
+if (choice == 1) % Gray world assumption
+    avgRGB = shiftdim(mean(mean(img)),1);
     grayValue = mean(avgRGB);
     scaleRGB = grayValue ./ avgRGB;
-    
-    scaleRGB = scaleRGB ./ scaleRGB(2);
-    
+ 
     description = 'Gray world assumption';
 else
-    if (choice == 2)
-        grayValue = mean(avgRGB);
+    if (choice == 2) % Manual white balancing
         whitePixel = impixel(img);
+        grayValue = mean(whitePixel);
         scaleRGB = grayValue ./ whitePixel;
-        scaleRGB = scaleRGB ./ scaleRGB(2);
-        description = 'Normalized scaling factors';
+        
+        description = 'White balanced';
     end
 end
+
+% Normalization
+scaleRGB = scaleRGB ./ scaleRGB(2);
 
 balanced = img;
 balanced(:,:,1) = balanced(:,:,1) * scaleRGB(1);
 balanced(:,:,2) = balanced(:,:,2) * scaleRGB(2);
 balanced(:,:,3) = balanced(:,:,3) * scaleRGB(3);
 
-figure(2);
+figure;
 imshow(balanced);
 title(description);
