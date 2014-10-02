bb28842f60c0f1186d050b6b79d079d3ce7b73d7
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-10-02 21:32:32 +0200

    Removed imshow within balance method

--- project-01/Material/Matlab Example/balance.m
+++ project-01/Material/Matlab Example/balance.m
@@ -6,15 +6,11 @@ if (choice == 1) % Gray world assumption
     avgRGB = shiftdim(mean(mean(img)),1);
     grayValue = mean(avgRGB);
     scaleRGB = grayValue ./ avgRGB;
- 
-    description = 'Gray world assumption';
 else
     if (choice == 2) % Manual white balancing
         whitePixel = impixel(img);
         grayValue = mean(whitePixel);
         scaleRGB = grayValue ./ whitePixel;
-        
-        description = 'White balanced';
     end
 end
 
@@ -25,7 +21,3 @@ balanced = img;
 balanced(:,:,1) = balanced(:,:,1) * scaleRGB(1);
 balanced(:,:,2) = balanced(:,:,2) * scaleRGB(2);
 balanced(:,:,3) = balanced(:,:,3) * scaleRGB(3);
-
-figure;
-imshow(balanced);
-title(description);
