f9e182a8d96a0619339aa52847bbecd934184d0d
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-12-09 16:44:58 +0100

    Visualization of frames with a movie added; visualization of EPIs done.

---
+++ project-06/start.m
@@ -0,0 +1,45 @@
+%% Project 6
+
+% find the folder
+% folder = uigetdir();
+folder = 'lightfields/pink_animal/';
+if ~(folder==0)
+    % get the names of all image files
+    searchFor = strcat(folder, '/*.png');
+    dirListing = dir(searchFor);
+    % get number of images
+    numFrames = length(dirListing);
+    % open images and store it in array
+    % first image
+    % use full path because the folder may not be the active path
+    fileName = fullfile(folder,dirListing(1).name);
+    img1 = imread(fileName);
+    images = zeros([size(img1) numFrames]);
+
+    for frame = 1:numFrames
+        fileName = fullfile(folder,dirListing(frame).name)
+        images(:,:,:,frame) = im2double(imread(fileName));
+    end
+
+    % Display images as a movie
+    for frame = 1:numFrames
+        F(frame) = im2frame(images(:,:,:,frame));
+    end
+    movie(F);
+end
+
+%% Show EPI's
+scanlines = [400 50];
+for i=1:length(scanlines)
+    scanline = scanlines(i);
+    permuted = permute(images,[4 2 3 1]);
+
+    figure((i-1)*2+1);
+    img = images(:,:,:,60);
+    imshow(img); hold on; plot([0 size(img,2)], [scanline scanline],'r','LineWidth',1);
+    title(['Row #',num2str(scanline)]);
+
+    figure((i-1)*2+2);
+    imshow(permuted(:,:,:,scanline));
+    title(['EPI #',num2str(scanline)]);
+end
