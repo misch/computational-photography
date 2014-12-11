53e68bade840813c42c07a5f683c94ea894afdcf
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-12-11 13:40:06 +0100

    Gaussian filtering before interpolation.

--- project-06/viewReconstruction.m
+++ project-06/viewReconstruction.m
@@ -42,3 +42,35 @@ imshow(finalImg);
 titleStr1 = sprintf('Sheared interpolation of views %d and %d',cameraView1, cameraView2);
 titleStr2 = sprintf('%d pixels shear',shear);
 title([{titleStr1, titleStr2}]);
+
+%% Filtering with Gaussian
+shearedEPIs = zeros(size(EPIs));
+center = 60;
+k = 5;
+shear = 5;
+sigmaHorizontal = 5;
+sigmaVertical = 45;
+
+for nEPI = 1:size(EPIs,4)
+    shearedEPIs(:,:,:,nEPI) = shearEPI(EPIs(:,:,:,nEPI),center,shear);
+end
+
+cameraView1 = center - floor(k/2);
+cameraView2 = center + ceil(k/2);
+numEPIs = size(EPIs,4);
+
+gaussHorizontal = fspecial('gaussian',[1 ceil(3*sigmaHorizontal)],sigmaHorizonal);
+gaussVertical = fspecial('gaussian',[ceil(3*sigmaVertical) 1],sigmaVertical);
+for i = 1:numEPIs
+    epi = shearedEPIs(:,:,:,i);
+
+    epi = imfilter(epi,gaussHorizontal); epi = imfilter(epi,gaussVertical);
+    finalImg(i,:,:) = 0.5*(epi(cameraView1,:,:) + epi(cameraView2,:,:));
+end
+
+figure();
+imshow(finalImg);
+titleStr1 = sprintf('Sheared interpolation of views %d and %d',cameraView1, cameraView2);
+titleStr2 = sprintf('%d pixels shear, sigma horizontal = %d, sigma vertical = %d',shear,sigmaHorizontal,sigmaVertical);
+title([{titleStr1, titleStr2}],'FontSize',13);
+imtool(finalImg);
